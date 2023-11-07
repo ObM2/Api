76bb9afada9ced4a9e4096bcdcf8acfd7623b0e0
jriko9 <136967698+jriko9@users.noreply.github.com>
2023-11-07 01:00:54 +0100

    Create intro.php

---
+++ api/intro.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width">
+  </head>
+  <body>
+    <video controls="" autoplay="" name="media">
+      <source src="https://icethenext.com/sky/img/blank.mp4" type="video/mp4">
+    </video>
+  </body>
+</html>
